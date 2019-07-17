a058792e0a284fef3c6a099e16572068966d073e
David <david.lasezkay@gmail.com>
2019-07-17 11:30:46 -0700

    defined cartsummary and displays cart info including total price and basic item info

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -2,11 +2,37 @@ import React from 'react';
 import CartSummaryItem from './cart-summary-item';
 
 export default class CartSummary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.backToCatalog = this.backToCatalog.bind(this);
+    this.calculateOrderTotal = this.calculateOrderTotal.bind(this);
+  }
+
+  backToCatalog() {
+    this.props.viewState('catalog', {});
+  }
+
+  calculateOrderTotal() {
+    let total = null;
+    let convertedNum = null;
+
+    this.props.itemsInCart.forEach(item => {
+      total += item.price;
+    });
+
+    convertedNum = (total / 100).toFixed(2);
+    return convertedNum;
+  }
 
   render() {
+    const angle = '<';
+
     if (this.props.itemsInCart.length > 0) {
       return (
         <div>
+          <p className="text-muted" onClick={this.backToCatalog} style={{ cursor: 'pointer', marginTop: '20px', marginBottom: '20px' }}>{angle} Back to Catalog</p>
+          <h2>My Cart</h2>
           {
             this.props.itemsInCart.map(item => {
               return (
@@ -14,12 +40,13 @@ export default class CartSummary extends React.Component {
               );
             })
           }
+          <p>Item Total {this.calculateOrderTotal()}</p>
         </div>
       );
     } else {
       return (
         <div>
-                    Your cart is empty!!
+                Your cart is empty!!
         </div>
       );
     }
