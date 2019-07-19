eacb8b2f8439e55a6b6964548c9e3124fb15a2d5
David <david.lasezkay@gmail.com>
2019-07-19 16:27:41 -0700

    wip did some set up but not much progress made

--- server/public/api/functions.php
+++ server/public/api/functions.php
@@ -1,14 +1,16 @@
 <?php
 
-function handleError($error) {
-    http_response_code(500);
-    $output=[
-        'success' => false,
-        'error' => $error->getMessage()
-    ];
+if (!function_exists('handleError')){
+    function handleError($error) {
+        http_response_code(500);
+        $output=[
+            'success' => false,
+            'error' => $error->getMessage()
+        ];
 
-    $json_output = json_encode($output);
-    print($json_output);
+        $json_output = json_encode($output);
+        print($json_output);
+    }
 }
 
 ?> 
--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,20 +1,35 @@
 <?php
 
+header('Content-Type: application/json');
+
 require_once('db_connection.php');
 require_once('functions.php');
 
-// set_exception_handler('handleError');
+set_exception_handler('handleError');
 
-$output = file_get_contents('dummy-products-list.json');
+$query = "SELECT * FROM Products";
 
-// print($output);
+$result = mysqli_query($conn, $query);
 
-header('Content-Type: application/json');
-
-if (empty($_GET['id'])) {
-  readfile('dummy-products-list.json');
-} else {
-  readfile('dummy-product-details.json');
+if (!$result){
+  throw new Exception('error with query: '.mysqli_error($conn));
 }
 
+$data = [];
+
+while($row = mysqli_fetch_assoc($result)){
+  $data[] = $row;
+}
+
+print(json_encode($data));
+
+// $output = file_get_contents('dummy-products-list.json');
+
+// if (empty($_GET['id'])) {
+//   readfile('dummy-products-list.json');
+// } else {
+//   readfile('dummy-product-details.json');
+// }
+
 ?>
+
